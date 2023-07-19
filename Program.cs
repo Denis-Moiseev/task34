7233c995a235520a27209b4ecd56523d2ac68314
Denis-Moiseev <mrtesla666@gmail.com>
2023-07-19 16:36:01 +0800

    finish

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿int GetQuantityEvenNum(int[] array)
+{
+    int evenNum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            evenNum++;
+        }
+    }
+    return evenNum;
+}
+
+int[] GetRandomArray(int Length, int min, int max)
+{
+    int[] array = new int[Length];
+    for (int i = 0; i < Length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+    return array;
+}
+
+int[] array = GetRandomArray(4, 100, 1000);
+Console.Write($"[{String.Join(",",array)}] -> ");
+Console.Write(GetQuantityEvenNum(array));
